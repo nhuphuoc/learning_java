985aabd97c1e398b311f0421717ab634413e8cf0
nhuphuoc.bui <nhuphuoc.bui@gmail.com>
2024-11-07 21:56:43 +0700

    feature 1

--- src/main/java/org/example/learning_java/javacore/LearnJavaCore.java
+++ src/main/java/org/example/learning_java/javacore/LearnJavaCore.java
@@ -3,5 +3,9 @@ package org.example.learning_java.javacore;
 public class LearnJavaCore {
 	public static void main(String[] args){
     System.out.println("Hello World");
+		feature1();
 	}
+	public static void feature1() {
+    System.out.println("Feature 1");
+  }
 }
---
+++ src/main/java/org/example/learning_java/javacore/Test.java
@@ -0,0 +1,4 @@
+package org.example.learning_java.javacore;
+
+public class Test {
+}
